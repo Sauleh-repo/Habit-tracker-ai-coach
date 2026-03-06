32e99018e90f7a1f2fdf577f1f5b020e9d69372a
Saleh Bakhshi <salehbakhshi321@gmail.com>
2026-03-06 20:42:08 +0530

    Fix: Update CORS origins to allow GCP frontend

--- sql_app/main.py
+++ sql_app/main.py
@@ -83,12 +83,16 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://storage.googleapis.com",
+        "http://storage.googleapis.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 def get_db():
     db = SessionLocal()
     try:
